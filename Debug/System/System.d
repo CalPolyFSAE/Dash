2d391e5826e001c67c1c99688d6a8617a3141a2c
salticids <graydon@seewasp.com>
2019-11-14 15:35:12 -0800

    cleaned for 2019

--- Debug/System/System.d
+++
@@ -1,5 +0,0 @@
-System/System.o System/System.d: \
- /Users/mugetronblue/Documents/MKELibrary/System/System.cpp \
- /Users/mugetronblue/Documents/MKELibrary/System/System.h
-
-/Users/mugetronblue/Documents/MKELibrary/System/System.h:
