ab44d4b9b1f070ed37f9122f420354d776ffa91a
mazda4952 <mazda4952@users.noreply.github.com>
2020-02-01 12:10:43 -0800

    added make_unique

--- dash_app/include/util.h
+++ dash_app/include/util.h
@@ -9,6 +9,7 @@
 #define DASH_UTIL_H
 
 #include <stdint.h>
+#include <bits/unique_ptr.h>
 
 namespace dash_util {
 
@@ -16,5 +17,17 @@ uint32_t get_clock_cycle_count();
 void delay_us(uint32_t us);
 
 }
+
+// add make_unique (c++ 14 feature)
+#if __cplusplus <= 201103L
+
+namespace std {
+    template<typename T, typename... Args>
+    unique_ptr<T> make_unique(Args&&... args) {
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+    }
+}
+
+#endif // make_unique
 
 #endif // DASH_UTIL_H
